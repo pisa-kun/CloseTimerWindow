bf14adaca6c451e5d21b153cc552d0871acafe2a
pisa <pisa0401mst@gmail.com>
2020-12-22 03:48:01 +0900

    firstcommit

---
+++ CloseTimerWindow/AutoCloseWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CloseTimerWindow
+{
+    /// <summary>
+    /// AutoCloseWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class AutoCloseWindow : Window
+    {
+        private DispatcherTimer timer;
+
+
+        public AutoCloseWindow()
+        {
+            InitializeComponent();
+
+            var desktopWorkingArea = SystemParameters.WorkArea;
+            this.Left = desktopWorkingArea.Right - this.Width;
+            this.Top = desktopWorkingArea.Bottom - this.Height;
+        }
+
+        /// <summary>
+        /// https://takap-tech.com/entry/2017/09/09/225342
+        /// </summary>
+        /// <param name="message"></param>
+        public void Show(string message, int elapsedSecond)
+        {
+            this.Content.Text = message;
+            this.Show();
+            this.TimerText.Text = "0秒経過中";
+            int num = 0;
+
+            // タイマーの間隔(ミリ秒)
+            this.timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(1000)
+            };
+
+            // 5秒以下なら最低5秒もたせる
+            if(elapsedSecond < 5)
+            {
+                elapsedSecond = 5;
+            }
+
+            // タイマーの処理
+            this.timer.Tick += (s, e) =>
+            {
+                if (num < elapsedSecond)
+                {
+                    num++;
+                    this.TimerText.Text = num.ToString() + "秒経過中";
+                }
+                else
+                {
+                    this.TimerText.Text = "windowをcloseします";
+                    this.timer.Stop();
+                    this.Close();
+                }
+            };
+
+            // タイマーを開始する
+            timer.Start();
+        }
+    }
+}
---
+++ CloseTimerWindow/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace CloseTimerWindow
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // タイマーの間隔(ミリ秒)
+            // 10秒定期
+            // UI処理なのでDispatcherTimerである必要がある
+            var timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(10000)
+            };
+
+            // タイマーの処理
+            timer.Tick += (sender, e) =>
+            {
+                var cw = new AutoCloseWindow();
+                cw.Show("10秒定期に作成されるwindowです。\n15秒後消えます", 15);
+            };
+
+            // タイマーを開始する
+            timer.Start();
+        }
+
+        private void top_Click(object sender, RoutedEventArgs e)
+        {
+            var cw = new AutoCloseWindow();
+            cw.Show(this.top.Content.ToString(), 5);
+        }
+
+        private void bottom_Click(object sender, RoutedEventArgs e)
+        {
+            var cw = new AutoCloseWindow();
+            cw.Show(this.bottom.Content.ToString(), 5);
+        }
+    }
+}
